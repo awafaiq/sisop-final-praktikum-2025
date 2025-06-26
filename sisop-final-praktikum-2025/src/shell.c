08e385e36b790a0e52f7b51cb941ae1f9f6cfa71
Danish Faiq Ibad Yuadi <142654390+awafaiq@users.noreply.github.com>
2025-06-26 08:43:46 +0700

    Add files via upload

--- sisop-final-praktikum-2025/src/shell.c
+++ sisop-final-praktikum-2025/src/shell.c
@@ -71,16 +71,125 @@ void printCWD(byte cwd) {
 
 // TODO: 5. Implement parseCommand function
 void parseCommand(char* buf, char* cmd, char arg[2][64]) {
+  int i = 0; 
+  int j = 0;
+  int k = 0;
+
+  // Kosongkan semua
+  clear(cmd, 64);
+  clear(arg[0], 64);
+  clear(arg[1], 64);
+
+  // Salin cmd
+  while (buf[i] != ' ' && buf[i] != '\0') {
+    cmd[i] = buf[i];
+    i++;
+  }
+  cmd[i] = '\0';
+
+  if (buf[i] == ' ') i++; 
+  
+  // Argumen pertama
+  while (buf[i] != ' ' && buf[i] != '\0') {
+    arg[0][j++] = buf[i++];
+  }
+  arg[0][j] = '\0';
+
+  if (buf[i] == ' ') i++; 
+
+  // Argumen kedua
+  while (buf[i] != '\0') {
+    arg[1][k++] = buf[i++];
+  }
+  arg[1][k] = '\0';
 }
+
 
 // TODO: 6. Implement cd function
-void cd(byte* cwd, char* dir_name) {
+void cd(byte* cwd, char* dirname) {
+struct node_fs node_fs_buf;
+struct node_item* node;
+int i;
+bool found = false;
+
+readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+// cd ..
+if (strcmp(dirname, ".."))
+{
+  if (*cwd != FS_NODE_P_ROOT)
+  {
+    *cwd = node_fs_buf.nodes[*cwd].parent_index;
+    found = true;
+  }
+  return;
+} 
+
+// cd /
+if (strcmp(dirname, "/"))
+{
+  *cwd = FS_NODE_P_ROOT;
+  found = true;
+  return;
+}
+
+//cd dirname
+
+for (i = 0; i < FS_MAX_NODE; i++)
+{
+  if (node_fs_buf.nodes[i].parent_index == *cwd && strcmp(node_fs_buf.nodes[i].node_name, dirname))
+  {
+    if (node_fs_buf.nodes[i].data_index == FS_NODE_D_DIR)
+    {
+      *cwd = i;
+      found = true;
+      return;
+    }
+  }
+}
+
+if (!found)
+{
+  printString("Directory tidak ditemukan!\n");
+  return;
+}
 
 }
+
 
 // TODO: 7. Implement ls function
 void ls(byte cwd, char* dir_name) {
+  struct node_fs node_fs_buf;
+  byte target = cwd;
+  int i;
 
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  if (strlen(dir_name) > 0 && strcmp(dir_name, ".") == false) {
+    bool found = false;
+    for (i = 0; i < FS_MAX_NODE; i++) {
+      if (node_fs_buf.nodes[i].parent_index == cwd &&
+          node_fs_buf.nodes[i].data_index == FS_NODE_D_DIR &&
+          strcmp(node_fs_buf.nodes[i].node_name, dir_name)) {
+        target = i;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      printString("Directory not found\n");
+      return;
+    }
+  }
+
+  for (i = 0; i < FS_MAX_NODE; i++) {
+    if (node_fs_buf.nodes[i].parent_index == target) {
+      printString(node_fs_buf.nodes[i].node_name);
+      printString("\n");
+    }
+  }
 }
 
 
